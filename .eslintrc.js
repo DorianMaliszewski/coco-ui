9b499e51c44161f4c4e4e0f8163858545d1ce518
Dorian Maliszewski <dmaliszewski@scaleway.com>
2022-05-31 21:02:33 +0200

    chore: update deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,9 @@
 module.exports = {
   env: {
     node: true,
-    commonjs: true,
+    commonjs: true
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended', 'plugin:storybook/recommended'],
   parser: '@typescript-eslint/parser',
   // Specifies the ESLint parser
   parserOptions: {
@@ -17,17 +12,19 @@ module.exports = {
     sourceType: 'module',
     // Allows for the use of imports
     ecmaFeatures: {
-      jsx: true, // Allows for the parsing of JSX
-    },
+      jsx: true // Allows for the parsing of JSX
+
+    }
   },
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     '@typescript-eslint/no-explicit-any': 'off',
-    'react/prop-types': 'off',
+    'react/prop-types': 'off'
   },
   settings: {
     react: {
-      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
-    },
-  },
-}
+      version: 'detect' // Tells eslint-plugin-react to automatically detect the version of React to use
+
+    }
+  }
+};
--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,6 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  framework: '@storybook/react',
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
@@ -11,7 +12,6 @@ module.exports = {
         },
       },
     },
-    '@storybook/react',
     '@storybook/addon-a11y',
   ],
   core: {
